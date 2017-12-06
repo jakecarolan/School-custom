6ae1a9687423ff5ef9f9325cd6b266c8d1a7950c
jakecarolan <34284212+jakecarolan@users.noreply.github.com>
2017-12-06 20:10:14 +0200

    Add new skill pelmeni

--- scripts/vscripts/modifier_pelmeni.lua
+++ scripts/vscripts/modifier_pelmeni.lua
@@ -2,12 +2,12 @@ modifier_pelmeni = class({})
 
 function modifier_pelmeni:DeclareFunctions()
     local funcs_array = {
-	MODIFIER_PROPERTY_STATS_STRENGTH_BONUS
+	MODIFIER_PROPERTY_EXTRA_STRENGTH_BONUS
 	} 
 	
 	return funcs_array
 end
 
-function modifier_pelmeni:GetModifierBonusStats_Strength(params)
-	return 10, 15, 20, 25;
+function modifier_pelmeni:GetModifierExtraStrengthBonus(params)
+	return 20
 end
